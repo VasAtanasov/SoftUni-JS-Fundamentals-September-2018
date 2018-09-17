7f739fa76921bd38dfa166664cffd2547732429b
Vasil Atanasov <vas.atanasov@gmail.com>
2018-09-17 19:35:32 +0300

    renamed

--- L03JavaScriptSyntax/ex05StriongOfNumbers.js
+++
@@ -1,9 +0,0 @@
-function stringOfNumbers(number) {
-    let string = "1";
-    for (let i = 2; i <= number; i++) {
-        string += i;
-    }
-    console.log(string);
-}
-
-stringOfNumbers('11');
